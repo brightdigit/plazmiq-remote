ccf2a8cfd7b17c9f7400340404c29e70bc82e292
Leo Dion <leogdion@brightdigit.com>
2016-05-12 13:12:45 -0400

    ssh connection works

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var AWS = require('aws-sdk'); 
 var SSH = require('simple-ssh');
+var waitForPort = require('wait-for-port');
 
 AWS.config.loadFromPath('./.credentials/credentials.json');
 
 Array.prototype.flatMap = function(lambda) { 
-    return Array.prototype.concat.apply([], this.map(lambda)); 
+  return Array.prototype.concat.apply([], this.map(lambda)); 
 };
 
 var ec2 = new AWS.EC2();
@@ -18,17 +19,17 @@ var params = {
     Enabled: false /* required */
   },
   InstanceType: 't2.micro',
-      SubnetId: 'subnet-3f4ef85b',
+  SubnetId: 'subnet-3f4ef85b',
   BlockDeviceMappings: [
-    {
-      Ebs: {
-        DeleteOnTermination: true,
-        VolumeSize: 500,
-        VolumeType: 'sc1'
-      },
-      DeviceName: '/dev/sdf'
+  {
+    Ebs: {
+      DeleteOnTermination: true,
+      VolumeSize: 500,
+      VolumeType: 'sc1'
     },
-    /* more items */
+    DeviceName: '/dev/sdf'
+  },
+  /* more items */
   ],
   KeyName : "aws_ec2",
   SecurityGroupIds: ['sg-06c4cc61']
@@ -36,6 +37,22 @@ var params = {
 
 var pemKey = fs.readFileSync(".credentials/aws_ec2.pem").toString();
 
+var terminateInstances = 
+                (function (instanceIds) {
+                  var params = {
+                    InstanceIds : instanceIds
+                  };
+                  console.log("Terminating Instances...");
+                  ec2.terminateInstances(params, function(err, data) {
+                    if (err) console.log(err, err.stack); // an error occurred
+                    else     {
+                      ec2.waitFor('instanceTerminated', params, function(err, data) {
+                        if (err) console.log(err, err.stack); // an error occurred
+                        else     console.log(data);           // successful response
+                      });
+                    }
+                  });
+                });
 console.log("Requesting Instances...");
 ec2.runInstances(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
@@ -52,52 +69,64 @@ ec2.runInstances(params, function(err, data) {
         var ipAddresses = data.Reservations.flatMap( function (_) {return _.Instances.map(function (_) {
           return _.PublicIpAddress;
         })});
-        console.log(ipAddresses);
-        var sshConfig = {
-          host: ipAddresses.shift(),
-          user: "ec2-user",
-          key: pemKey
+        console.log("Waiting for Network Availablity...");
+        var params = {
+          'KeyNames' : ['aws_ec2']
         };
-        console.log(sshConfig);
-        var ssh = new SSH(sshConfig);
-        ssh.on('error', function(err) {
-            console.log('Oops, something went wrong.');
+        var ipAddress = ipAddresses.shift();
+        var start = new Date();
+        waitForPort(ipAddress, 22, { numRetries: 4, retryInterval: 30000 }, function(err) {
+          if (err) {
             console.log(err);
-            ssh.end();
-        });
-        ssh.exec('echo $PATH', {
-            out: function(stdout) {
+            terminateInstances(instanceIds);
+          } else {
+              var end = new Date();
+            console.log("Connecting to Instance...")
+            var sshConfig = {
+              host: ipAddress,
+              user: "ec2-user",
+              key: pemKey
+            };
+            var ssh = new SSH(sshConfig);
+            ssh.on('error', function(err) {
+              console.log('Oops, something went wrong.');
+              console.log(err);
+              ssh.end();
+              terminateInstances(instanceIds);
+            });
+            ssh.on('ready', function (){
+              console.log(end-start, "Milliseconds")
+            });
+            ssh.exec('pwd', {
+              out: function(stdout) {
                 console.log(stdout);
-            }
-        }).start();
+              },
+              exit: function(code, stdout, stderr) {
+                // sudo mkswap /dev/xvdf
+                // sudo swapon /dev/xvdf
 
-        // sudo mkswap /dev/xvdf
-        // sudo swapon /dev/xvdf
+                // yum -y install git
+                // curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+                // source ~/.bashrc
 
-        // yum -y install git
-        // curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-        // source ~/.bashrc
+                // install ssh keys
+                // install credentials
 
-        // install ssh keys
-        // install credentials
-
-        // git clone
-        // 
-        (function () {
-          var params = {
-            InstanceIds : instanceIds
-          };
-          console.log("Terminating Instances...");
-          ec2.terminateInstances(params, function(err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
-            else     {
-              ec2.waitFor('instanceTerminated', params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else     console.log(data);           // successful response
-              });
-            }
-          });
-        })()
+                // git clone
+                // 
+                console.log(stdout);
+                console.log(stderr);
+                console.log(code);
+                console.log("done");
+                ssh.end();
+                terminateInstances(instanceIds);
+              }
+            }).start();
+          }
+          // .. now we know that (at least for now) my-host.com is
+          // listening on port 22
+        });
+        
       }
     });
   }
