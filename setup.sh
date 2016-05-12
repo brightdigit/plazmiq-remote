2597b56d57a4345292f7c29cceb39ab67629d752
Leo Dion <leogdion@brightdigit.com>
2016-05-12 09:39:06 -0400

    working on setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo mkswap /dev/xvdf
+sudo swapon /dev/xvdf
+sudo yum -y install git
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+source ~/.bashrc
+git clone $1 source
+cd source
+nvm install
+nvm use
+make
