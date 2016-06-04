2ff9445f093fcfe93c6053ee2cc712779b09adef
leogdion <leogdion@brightdigit.com>
2016-06-04 16:33:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo mkswap /dev/xvdf
 sudo swapon /dev/xvdf
 sudo yum -y install jsawk
-sudo yum -y install git
+sudo yum -y install git bzip2
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 source ~/.bashrc
 git clone $1 source
